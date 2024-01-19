9bb0f1c9a7eb2abb463e208d3c93e02998f3f3ab
Amon De Shir <amonsir29@gmail.com>
2024-01-19 01:08:09 +0100

    add input

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,19 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-
-import './index.css'
+import './index.css';
+import { render } from 'solid-js/web';
 import { Header } from './components/header/header';
-import { Center } from './components/center/center';
-import { Page } from './components/page/page';
+import { SongList } from './components/song-list/song-list';
+import { MainPage } from './components/main-page/main-page';
 
 const root = document.getElementById('root')
 
 const App = () => (
   <>
     <Header />
-    <Center justifyContent='space-between'>
-      <Page color='main-dark' width='380px' title='Search'>Data</Page>
-      <Page title='Main'>Main</Page>
-    </Center>
+    <div class="main">
+      <SongList />
+      <MainPage />
+    </div>
   </>
 );
 
